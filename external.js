acd3274ddc060f4e1ef9d7702d8270aa9b5bfb5c
MRK <135884817+Rkhan2026@users.noreply.github.com>
2023-07-01 12:19:49 +0530

    function called playRound is created to play a single round as of now

--- external.js
+++ external.js
@@ -1,3 +1,19 @@
 function getComputerChoice() {
     return "rock";
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection="paper" && computerSelection="rock") {
+        return "You Win! Paper beats Rock";
+    }
+    if (playerSelection="scissor" && computerSelection="rock") {
+        return "You lose! Rock beats scissor";
+    }
+    if (playerSelection="scissor" && computerSelection="paper") {
+        return "You Win! scissor beats paper";
+    }
+  }
+   
+  const playerSelection = "paper";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
