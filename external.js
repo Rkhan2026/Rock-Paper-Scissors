df4eef23e1f21d102b4dac4ee6b9f5d0b63a3f14
MRK <135884817+Rkhan2026@users.noreply.github.com>
2023-07-01 18:28:01 +0530

    added func and way to give 5 round match result

--- external.js
+++ external.js
@@ -11,18 +11,15 @@ function getComputerChoice() {
     return val;
 }
 
-var playerSelect = prompt("Enter Choice");
-var lcrp = playerSelect.toLowerCase();
-playerSelect = lcrp;
-var computerSelect = getComputerChoice();
+var pscore=0; var cscore=0;
 
 function playRound(playerSelect, computerSelect) {
     if (playerSelect=="paper" && computerSelect=="rock") {
-        console.log("You win! Paper beats Rock");
+        console.log("You win! Paper beats Rock"); pscore++;
     } else if (playerSelect=="rock" && computerSelect=="scissors") {
-        console.log("You win! Rock beats Scissors");
+        console.log("You win! Rock beats Scissors"); pscore++;
     } else if (playerSelect=="scissors" && computerSelect=="paper") {
-        console.log("You win! Scissors beats paper");
+        console.log("You win! Scissors beats paper"); pscore++;
     } if (playerSelect=="paper" && computerSelect=="paper") {
         console.log("It's A Tie");
     } else if (playerSelect=="rock" && computerSelect=="rock") {
@@ -30,12 +27,33 @@ function playRound(playerSelect, computerSelect) {
     } else if (playerSelect=="scissors" && computerSelect=="scissors") {
         console.log("It's A Tie");
     } if (playerSelect=="rock" && computerSelect=="paper") {
-        console.log("You Lose! Paper beats Rock");
+        console.log("You Lose! Paper beats Rock"); cscore++;
     } else if (playerSelect=="scissors" && computerSelect=="rock") {
-        console.log("You Lose! Rock beats Scissors");
+        console.log("You Lose! Rock beats Scissors"); cscore++:
     } else if (playerSelect=="paper" && computerSelect=="scissors") {
-        console.log("You Lose! Scissors beats paper");
+        console.log("You Lose! Scissors beats paper"); cscore++;
     }
 }
 
-playRound(playerSelect,computerSelect);
+function game() {
+    var playerSelect = prompt("Enter Choice");
+    var lcrp = playerSelect.toLowerCase();
+    playerSelect = lcrp;
+    var computerSelect = getComputerChoice();
+    playRound(playerSelect,computerSelect);
+}
+
+game();
+game();
+game();
+game();
+game();
+
+if (pscore>cscore) {
+    console.log("Player Wins Against Computer");
+} else if (cscore>pscore) {
+    console.log("Computer Wins Against Player");
+} else if (cscore==pscore) {
+    console.log("Tie Between Player And Computer");
+} 
+
